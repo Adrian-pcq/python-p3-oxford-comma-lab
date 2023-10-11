3eb85d4bb66f508b4e548efab2ce645c1eee62c8
Adrian <adrianqueizan@gmail.com>
2023-10-11 16:54:10 -0400

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items)==1:
+        return  " ".join(items)
+    
+    elif len(items)==2:
+        items[-1]= "and "+items[-1]
+        return " ".join(items)
+
+    else:
+        items[-1]= "and "+items[-1]
+        return ", ".join(items)
+
